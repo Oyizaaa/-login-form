ff4dd647d0e3f1a87c85381cd177de509decc6e1
Oyizaaa <siyakaaisha@gmail.com>
2023-09-08 15:20:52 +0100

    calculator

---
+++ calculatorr/script.js
@@ -0,0 +1,28 @@
+function add() {
+    num1=parseInt(myform.num1.value);
+    num2=parseInt(myform.num2.value);
+    result=num1+num2;
+    myform.result.value=result;
+}
+
+function subtract() {
+    num1=parseInt(myform.num1.value);
+    num2=parseInt(myform.num2.value);
+    result=num1-num2;
+    myform.result.value=result;
+}
+
+function multiply() {
+    num1=parseInt(myform.num1.value);
+    num2=parseInt(myform.num2.value);
+    result=num1*num2;
+    myform.result.value=result;
+    
+}
+
+function divide() {
+    num1=parseInt(myform.num1.value);
+    num2=parseInt(myform.num2.value);
+    result=num1/num2;
+    myform.result.value=result;
+}
